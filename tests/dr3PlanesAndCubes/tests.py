97fe5bed79fc9f607f190e5f56667b96290c4ee6
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-10-05 08:40:52 -0600

    Escena Final

--- tests/dr3PlanesAndCubes/tests.py
+++ tests/dr3PlanesAndCubes/tests.py
@@ -16,9 +16,12 @@ from gllib.light import PointLight,AmbientLight
 
 #Material to use
 
-color1 = Material(diffuse = colorScale(0.8, 0.4, 0.25),specularity=16)
-color2 = Material(diffuse = colorScale(0.4, 0.4, 0.4),specularity=32)
+color1 = Material(diffuse = colorScale(116/255, 47/255, 0),specularity=16)
+color2 = Material(diffuse = colorScale(255/255, 156/255, 87/255),specularity=32)
 color3 = Material(diffuse = colorScale(0.2,0.6,0.8),specularity=32)
+color4 = Material(diffuse = colorScale(1, 1, 1),specularity=16)
+color5 = Material(diffuse = colorScale(1, 0, 0),specularity=16)
+color6 = Material(diffuse = colorScale(0, 1, 0),specularity=16)
 
 #Create our raytracer and spheres to build a snowman
 raytracerGl=Raytracer(int(512/2),int(512/2))
@@ -32,13 +35,23 @@ raytracerGl.ambientLight= AmbientLight(strength=0.1)
 # raytracerGl.sceneObjects.append( Sphere((-3, -3, -10), 1, mirror))
 
 
+
 #Planes
-raytracerGl.sceneObjects.append( Plane(position=(0,-3,0),normal=(0,1,0),material=color1))
+#Below
+raytracerGl.sceneObjects.append( Plane(position=(0,-3,0),normal=(0,1,0),material=color4))
+#Back
 raytracerGl.sceneObjects.append( Plane(position=(0,0,-10),normal=(0,0,1),material=color3))
+#Left
+raytracerGl.sceneObjects.append( Plane(position=(-3,0,0),normal=(1,0,0),material=color2))
+#Right
+raytracerGl.sceneObjects.append( Plane(position=(3,0,0),normal=(1,0,0),material=color2))
+#Top
+raytracerGl.sceneObjects.append( Plane(position=(0,3,0),normal=(0,1,0),material=color1))
+
 
 # AAABox
-# raytracerGl.sceneObjects.append( AABBox(position=(1.5,-1.5,-5),size=1.5,material=color3))
-# raytracerGl.sceneObjects.append( AABBox(position=(-1.5,-1.5,-5),size=1.5,material=color1))
+raytracerGl.sceneObjects.append( AABBox(position=(1,-1.5,-5),size=1,material=color5))
+raytracerGl.sceneObjects.append( AABBox(position=(-0.8,-1.3,-4),size=1.2,material=color6))
 
 
 raytracerGl.rtRender()
